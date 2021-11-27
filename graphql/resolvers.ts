411ad158d51cfeb0b35a873de97a3bee2c4681a0
unknown <byron789_@hotmail.com>
2021-11-27 13:40:52 -0500

    Logramos llamar campos de la bd atlas e insertar un usuario

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -3,10 +3,23 @@ import { UserModel } from "../models/users";
 const resolvers={
     Query:{
         Usuarios: async(paren,args)=>{
-            const usuarios =['pepito','omar'];
+            const usuarios = await UserModel.find();
             return usuarios;
         },
     },
+    Mutation:{
+        crearUsuario: async(parent,args)=>{
+            const user= await UserModel.create({
+                nombre:args.nombre,
+                apellido:args.apellido,
+                identificacion:args.identificacion,
+                correo:args.correo,
+                estado:args.estado,
+                rol:args.rol,
+            });
+            return user;
+        }
+    }
 };
     
 export {resolvers};
